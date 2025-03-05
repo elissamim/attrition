e2dc6ef6801822298827a7c8119f0efac2b0caa1
elissamim <elissamim@gmail.com>
2025-03-05 17:32:46 +0000

    (add) api

--- src/main.py
+++ src/main.py
@@ -1,12 +1,31 @@
-from fastapi import FastAPI
+import uvicorn
+from fastapi import FastAPI, Depends
 import joblib
 import pandas as pd
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 model = joblib.load("../model/attrition_classification_model.pkl")
 
+class InputData(BaseModel):
+    Age:int
+    Years_at_Company:int
+    Average_Monthly_Hours:int
+    Salary:int
+    Satisfaction_Level:float
+    Gender:str
+    Department:str
+    Promotion_Last_5Years:int
+
 @app.post("/predict/")
-def predict(data: list):
+def predict(data: List[InputData]):
     df = pd.DataFrame(data)
     prediction = model.predict(df)
     return {"prediction": prediction.tolist()}
+
+
+# @app.post("/predict/")
+# def predict(data: list[InputData]):  # Expecting a **list** of InputData
+#     return {"message": "Data received", "data": data}
+
